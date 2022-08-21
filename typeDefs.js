2a948b5123dabeb426e5a1f2f3b1065a09291065
ajr250571 <36195764+ajr250571@users.noreply.github.com>
2022-08-21 09:04:35 -0300

    typedefs

--- typeDefs.js
+++ typeDefs.js
@@ -42,32 +42,3 @@ type Mutation {
 }
 
 `
-
-/*
-type Msg {
-  code: String
-  message: String
-}
-
-type Task {
-  id: ID
-  title: String
-  description: String
-}
-
-input TaskInput {
-  title: String
-  description: String
-}
-
-type Query {
-  taskFindMany: [Task!]! 
-  taskFindUnique(id: ID!): Task
-}
-
-type Mutation {
-  taskCreate(task: TaskInput!): Task
-  taskDelete(id: ID!): Msg
-  taskUpdate(id: ID!, task: TaskInput): Task
-}
-*/
