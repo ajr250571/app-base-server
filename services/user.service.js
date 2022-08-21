5f4294cb40176be70aec3c1832699b405eac73b4
ajr250571 <36195764+ajr250571@users.noreply.github.com>
2022-08-21 19:45:13 -0300

    Control Errores

--- services/user.service.js
+++ services/user.service.js
@@ -1,38 +1,45 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 import * as bcrypt from "bcrypt";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export async function Users() {
-  const users = await prisma.user.findMany()
-  return users
+  const users = await prisma.user.findMany();
+  return users;
 }
 
 export async function User(id) {
-  const user = await prisma.user.findUnique({
-    where: {
-      id: Number(id),
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (user) {
+      return user;
+    } else {
+      return { error: "NOT_FOUND" };
     }
-  })
-  return user
+  } catch (e) {
+    console.log(e.code, e.meta);
+    return { error: e.code };
+  }
 }
 
 export async function CreateUser(user) {
   try {
-    const clave = await bcrypt.hash(user.password, 10)
+    const clave = await bcrypt.hash(user.password, 10);
     const userCreated = await prisma.user.create({
       data: {
         email: user.email,
         name: user.name || "",
         theme: user.theme || "dark",
-        password: clave
-      }
-    })
-    return userCreated
+        password: clave,
+      },
+    });
+    return userCreated;
   } catch (e) {
-    //console.log(e.code, e.meta)
-    console.log(e)
-    throw e
+    return { error: e.code };
   }
 }
 
@@ -40,23 +47,23 @@ export async function ValidUser(email, password) {
   const user = await prisma.user.findUnique({
     where: {
       email,
-    }
-  })
+    },
+  });
   if (user) {
-    const valid = await bcrypt.compare(password, user.password)
+    const valid = await bcrypt.compare(password, user.password);
     if (valid) {
       return {
         id: user.id,
         email: user.email,
         name: user.name,
         theme: user.theme,
-        isValid: true
-      }
+        isValid: true,
+      };
     } else {
-      return { isValid: false }
+      return { isValid: false };
     }
   } else {
-    return { isValid: false }
+    return { isValid: false };
   }
 }
 
@@ -67,11 +74,16 @@ export async function UpdateUser(id, user) {
         id: Number(id),
       },
       data: user,
-    })
-    return userUpdated
+    });
+    if (userUpdated) {
+      return userUpdated;
+    } else {
+      return { error: "NOT_FOUND" };
+    }
   } catch (e) {
-    console.log(e.code, e.meta)
-    throw e
+    console.log(e.code, e.meta);
+    //throw e;
+    return { error: e.code };
   }
 }
 
@@ -79,13 +91,13 @@ export async function DeleteUser(id) {
   try {
     const userDeleted = await prisma.user.delete({
       where: {
-        id: Number(id)
-      }
-    })
-    return userDeleted
+        id: Number(id),
+      },
+    });
+    return userDeleted;
   } catch (e) {
-    console.log(e.code, e.meta)
-    throw e
+    // console.log(e.code, e.meta);
+    // throw e;
+    return { error: e.code };
   }
 }
-
